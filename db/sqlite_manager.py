226d21029ee2ee0742f470b387032bf3b299fe57
Aravind <aravind19112003@gmail.com>
2025-11-09 21:33:02 +0530

    add sqlite manager

---
+++ db/sqlite_manager.py
@@ -0,0 +1,50 @@
+import sqlite3
+from pathlib import Path
+
+class SQLiteManager:
+    def __init__(self, db_path="database.db"):
+        self.db_path = Path(db_path)
+        self.conn = self._connect()
+        self._create_tables()
+
+    def _connect(self):
+        if not self.db_path.parent.exists():
+            self.db_path.parent.mkdir(parents=True, exist_ok=True)
+
+        if not self.db_path.exists():
+            conn = sqlite3.connect(str(self.db_path))
+            conn.close()
+
+        return sqlite3.connect(str(self.db_path))
+
+    def close(self):
+        if self.conn:
+            self.conn.close()
+            self.conn = None
+
+    def _create_tables(self):
+        cur = self.conn.cursor()
+        cur.execute(
+            """
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT
+            )
+            """
+        )
+        cur.execute(
+            """
+            CREATE TABLE IF NOT EXISTS api_keys (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                key VARCHAR,
+                user_id INTEGER,
+
+                FOREIGN KEY (user_id) REFERENCES users(id)
+                    ON UPDATE CASCADE
+                    ON DELETE CASCADE
+            )
+            """
+        )
+
+        self.conn.commit()
+        self.close()
