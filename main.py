f5b5012cdc5b358a093df78d366fda5b80b7c251
Aravind <aravind19112003@gmail.com>
2025-11-09 20:49:36 +0530

    add a post method & using pydantic models

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class User(BaseModel):
+    user_id: int
+    name: str
 
 @app.get("/health")
 def health():
     return {"Healthy!!"}
+
+@app.post("/user")
+def create_user(user: User):
+    return {"user id": user.user_id}
